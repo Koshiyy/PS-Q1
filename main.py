c9e6eac2bccafe445a649118aa39fdd1f8061632
Rodrigo Koshiyama <rodrigo.koshiyama@outlook.com>
2022-06-06 20:06:11 -0300

    Update main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-cap = cv.VideoCapture('./assets/images/q1B.mp4')
+cap = cv.VideoCapture('./assets/images/q1A.mp4')
 
 if (cap.isOpened() == False):
     print('Error opening video file')
@@ -9,23 +9,31 @@ if (cap.isOpened() == False):
 while (cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
-        frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-        edges = cv.Canny(frame_gray, 50, 150)
+        frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-        cnts = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        upper_red = np.array([180, 255, 255])
+        lower_red = np.array([160, 50, 50])
+        mask_red1 = cv.inRange(frame_hsv, lower_red, upper_red)
+
+        lower_red = np.array([0, 50, 50])
+        upper_red = np.array([10, 255, 255])
+        mask_red2 = cv.inRange(frame_hsv, lower_red, upper_red)
+
+        upper_blue = np.array([135, 255, 255])
+        lower_blue = np.array([90, 50, 50])
+        maskblue = cv.inRange(frame_hsv, lower_blue, upper_blue)
+
+        mask_red = mask_red1 + mask_red2
+
+        cnts = cv.findContours(mask_red, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
         for c in cnts:
             x, y, w, h = cv.boundingRect(c)
-            print(x, y)
             area = w * h
-            if area > 100000 and area < 104000:
-                rect = cv.rectangle(
-                    frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
-                if (x < 250):
-                    cv.putText(frame, 'Passou a barreira', (00, 185), cv.FONT_HERSHEY_SIMPLEX,
-                               1, (0, 255, 0), 2, cv.LINE_AA, False)
+            rect = cv.rectangle(
+                frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
         cv.imshow('Frame', frame)
 
         if cv.waitKey(25) & 0xFF == ord('q'):
